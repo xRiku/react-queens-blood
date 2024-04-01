b31f48a2d73eb9c55d53d7f38916a90c971b196b
Philipe A. Marques <philipe.marques@medprevonline.com>
2024-04-01 07:14:46 -0300

    fix: update points sum logic

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,7 +5,10 @@ import useCardStore from '../store/CardStore'
 import useBoardStore from '../store/BoardStore'
 
 export default function Board() {
-  const selectedCard = useCardStore((state) => state.selectedCard)
+  const [selectedCard, resetSelectedCard] = useCardStore((state) => [
+    state.selectedCard,
+    state.resetSelectedCard,
+  ])
   const rows = 3
   const cols = 7
   const tilesElements = new Array(rows)
@@ -15,35 +18,7 @@ export default function Board() {
     state.board,
     state.setBoard,
   ])
-  // const [tiles, setTiles] = useState<Tile[][]>(
-  //   matrix.map((value, index) => {
-  //     return value.map((value, columnIndex: number) => {
-  //       if (columnIndex === 0) {
-  //         return {
-  //           playerOnePoints: 0,
-  //           playerTwoPoints: 0,
-  //           playerOnePawns: 1,
-  //           playerTwoPawns: 0,
-  //         }
-  //       }
-  //       if (columnIndex === cols - 3) {
-  //         return {
-  //           playerOnePoints: 0,
-  //           playerTwoPoints: 0,
-  //           playerOnePawns: 0,
-  //           playerTwoPawns: 1,
-  //         }
-  //       }
-  //       return {
-  //         playerOnePoints: 0,
-  //         playerTwoPoints: 0,
-  //         playerOnePawns: 0,
-  //         playerTwoPawns: 0,
-  //       }
-  //     })
-  //   }),
-  // )
-  const playerOnePoints = new Array(3).fill(0)
+
   const playerTwoPoints = new Array(3).fill(0)
   const [isPlayerOneTurn, setIsPlayerOneTurn] = useState(true)
 
@@ -73,12 +48,7 @@ export default function Board() {
     return true
   }
 
-  function mapPawns(
-    card: Card,
-
-    rowIndex: number,
-    colIndex: number,
-  ) {
+  function mapPawns(card: Card, rowIndex: number, colIndex: number) {
     const transformedRowIndex = colIndex
     const transformedColIndex = -rowIndex
     const newTiles = [...tiles]
@@ -91,9 +61,16 @@ export default function Board() {
       }
 
       newTiles[newRow][newCol] = {
-        playerOnePoints: 0,
+        playerOnePoints:
+          tiles[newRow][newCol].playerOnePawns === -1
+            ? tiles[newRow][newCol].playerOnePoints
+            : 0,
         playerTwoPoints: 0,
-        playerOnePawns: tiles[newRow][newCol].playerOnePawns + 1,
+        playerOnePawns:
+          tiles[newRow][newCol].playerOnePawns !== -1 &&
+          tiles[newRow][newCol].playerOnePawns < 3
+            ? tiles[newRow][newCol].playerOnePawns + 1
+            : tiles[newRow][newCol].playerOnePawns,
         playerTwoPawns: 0,
       }
     }
@@ -101,7 +78,7 @@ export default function Board() {
     newTiles[rowIndex][colIndex] = {
       playerOnePoints: card.points,
       playerTwoPoints: 0,
-      playerOnePawns: 0,
+      playerOnePawns: -1,
       playerTwoPawns: 0,
     }
 
@@ -114,28 +91,30 @@ export default function Board() {
     colIndex: number,
   ) => {
     if (!canAddCardToPosition(selectedCard, position)) {
-      console.log('aa')
       return
     }
 
     if (!selectedCard) {
-      console.log('bb')
       return
     }
 
     const newTiles = mapPawns(selectedCard, rowIndex, colIndex)
 
     setTiles(newTiles)
+    resetSelectedCard()
   }
 
   for (let i = 0; i < rows; i++) {
+    const playerOnePoints = tiles[i]
+      .map((tile) => tile.playerOnePoints)
+      .reduce((acc, curr) => acc + curr, 0)
     tilesElements[i][0] = (
       <div
         className={`bg-white h-60 w-full flex items-center justify-center border-solid border-2 border-black`}
         key={`${i}-${0}`}
       >
         <div className="h-28 w-28 border-solid border-4 text-3xl border-green-300 rounded-full flex justify-center items-center">
-          {playerOnePoints[i]}
+          {playerOnePoints}
         </div>
       </div>
     )
@@ -159,13 +138,16 @@ export default function Board() {
           className={`${color} h-60 w-full border-solid border-4 hover:border-4 flex justify-center items-center border-black
            ${canAddCardToPosition(selectedCard, tiles[i][j - 1]) ? 'cursor-pointer  hover:border-green-400' : 'cursor-not-allowed hover:border-red-400'}
            transition duration-300 ease-out`}
-          // onMouseEnter={() => canAddCardToPosition(selectedCard, tiles[i][j])}
           onClick={() => handleCellClick(tiles[i][j - 1], i, j - 1)}
           key={`${i}-${j}`}
         >
           <div className="text-5xl font-bold text-center">
-            {tiles[i][j - 1] && '♟'.repeat(tiles[i][j - 1].playerOnePawns)}
-            {tiles[i][j - 1] && '♟'.repeat(tiles[i][j - 1].playerTwoPawns)}
+            {tiles[i][j - 1] &&
+              tiles[i][j - 1].playerOnePawns >= 0 &&
+              '♟'.repeat(tiles[i][j - 1].playerOnePawns)}
+            {tiles[i][j - 1] &&
+              tiles[i][j - 1].playerTwoPawns >= 0 &&
+              '♟'.repeat(tiles[i][j - 1].playerTwoPawns)}
           </div>
         </div>
       )
